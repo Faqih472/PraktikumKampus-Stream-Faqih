ca3b5a73410dc6e6738bdc9ed54aafe17c33a160
Faqih472 <mfaqih47289@gmail.com>
2025-06-15 18:43:46 +0700

    soal 7

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class _StreamHomePageState extends State<StreamHomePage> {
   int lastNumber = 0;
   late StreamController numberStreamController;
   late NumberStream numberStream;
+  late StreamTransformer transformer;
 
   void changeColor() {
     colorStream.getColors().listen((eventColor) {
@@ -54,6 +55,7 @@ class _StreamHomePageState extends State<StreamHomePage> {
     Random random = Random();
     int myNum = random.nextInt(10);
     numberStream.addNumberToSink(myNum);
+    // numberStream.addError();
   }
 
   @override
@@ -62,9 +64,24 @@ class _StreamHomePageState extends State<StreamHomePage> {
     numberStream = NumberStream();
     numberStreamController = numberStream.controller;
     Stream stream = numberStreamController.stream;
-    stream.listen((event) {
+
+    transformer =
+    StreamTransformer<int, int>.fromHandlers(handleData: (value, sink) {
+      sink.add(value * 10);
+    }, handleError: (error, trace, sink) {
+      sink.add(-1);
+    },
+        handleDone: (sink) {
+          sink.close();
+        });
+
+    stream.transform(transformer).listen((event) {
       setState(() {
         lastNumber = event;
+      });
+    }).onError((error) {
+      setState(() {
+        lastNumber = -1;
       });
     });
     // colorStream = ColorStream();
@@ -84,7 +101,7 @@ class _StreamHomePageState extends State<StreamHomePage> {
         title: Text('Stream Faqih'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body:SizedBox(
+      body: SizedBox(
         width: double.infinity,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
